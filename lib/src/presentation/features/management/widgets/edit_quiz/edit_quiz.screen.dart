4991133b9d8f45dfc8596616443078e45c10c255
Richard Maasri <richard.maasri@coodoo.de>
2023-10-10 11:12:46 +0200

    pinned header

--- lib/src/presentation/features/management/widgets/edit_quiz/edit_quiz.screen.dart
+++ lib/src/presentation/features/management/widgets/edit_quiz/edit_quiz.screen.dart
@@ -23,121 +23,127 @@ class EditQuizScreen extends ConsumerWidget {
               constraints: const BoxConstraints(maxWidth: 1000),
               child: Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        constraints: const BoxConstraints(maxWidth: 1000),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {},
-                              style: theme.elevatedButtonTheme.style?.copyWith(
-                                backgroundColor: MaterialStateProperty.all(kPrimaryColor),
-                              ),
-                              child: Text(
-                                'Quiz erstellen',
-                                style: theme.textTheme.labelMedium?.copyWith(color: kTextColorLight),
-                              ),
+                child: Column(
+                  children: [
+                    Container(
+                      constraints: const BoxConstraints(maxWidth: 1000),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: theme.elevatedButtonTheme.style?.copyWith(
+                              backgroundColor: MaterialStateProperty.all(kPrimaryColor),
                             ),
-                            const Spacer(),
-                            ElevatedButton(
-                              onPressed: () {},
-                              style: theme.elevatedButtonTheme.style?.copyWith(
-                                backgroundColor: MaterialStateProperty.all(kPrimaryColor),
-                              ),
-                              child: Text(
-                                'Verwerfen',
-                                style: theme.textTheme.labelMedium?.copyWith(color: kTextColorLight),
-                              ),
+                            child: Text(
+                              'Quiz erstellen',
+                              style: theme.textTheme.labelMedium?.copyWith(color: kTextColorLight),
                             ),
-                            const SizedBox(width: 4),
-                            ElevatedButton(
-                              onPressed: () async {
-                                await ref.read(editQuizControllerProvider(quiz.id).notifier).saveQuiz();
+                          ),
+                          const Spacer(),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: theme.elevatedButtonTheme.style?.copyWith(
+                              backgroundColor: MaterialStateProperty.all(kPrimaryColor),
+                            ),
+                            child: Text(
+                              'Verwerfen',
+                              style: theme.textTheme.labelMedium?.copyWith(color: kTextColorLight),
+                            ),
+                          ),
+                          const SizedBox(width: 4),
+                          ElevatedButton(
+                            onPressed: () async {
+                              await ref.read(editQuizControllerProvider(quiz.id).notifier).saveQuiz();
 
-                                if (context.mounted) context.pop();
-                              },
-                              style: theme.elevatedButtonTheme.style?.copyWith(
-                                backgroundColor: MaterialStateProperty.all(kSecondaryColor),
-                              ),
-                              child: Text(
-                                'Speichern',
-                                style: theme.textTheme.labelMedium?.copyWith(color: kTextColorLight),
+                              if (context.mounted) context.pop();
+                            },
+                            style: theme.elevatedButtonTheme.style?.copyWith(
+                              backgroundColor: MaterialStateProperty.all(kSecondaryColor),
+                            ),
+                            child: Text(
+                              'Speichern',
+                              style: theme.textTheme.labelMedium?.copyWith(color: kTextColorLight),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              constraints: const BoxConstraints(maxWidth: 1000),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(kCardBorderRadius),
+                                child: Column(
+                                  children: [
+                                    EditQuizTitleCard(quiz: quiz),
+                                    const SizedBox(height: 16),
+                                    ListView.separated(
+                                      shrinkWrap: true,
+                                      physics: const NeverScrollableScrollPhysics(),
+                                      itemCount: quiz.questions?.length ?? 0,
+                                      separatorBuilder: (context, index) => const SizedBox(height: 8),
+                                      itemBuilder: (context, index) {
+                                        switch (quiz.questions![index].type) {
+                                          case 'single':
+                                            return EditQuizSingleAnswerCard(
+                                              quiz: quiz,
+                                              question: quiz.questions![index],
+                                              onRemoveQuestion: () {
+                                                ref
+                                                    .read(editQuizControllerProvider(quiz.id).notifier)
+                                                    .removeQuestion(index: index);
+                                              },
+                                            );
+                                          case 'multiple':
+                                            return EditQuizMultipleAnswerCard(
+                                              quiz: quiz,
+                                              question: quiz.questions![index],
+                                              onRemoveQuestion: () {
+                                                ref
+                                                    .read(editQuizControllerProvider(quiz.id).notifier)
+                                                    .removeQuestion(index: index);
+                                              },
+                                            );
+                                          default:
+                                            return EditQuizSingleAnswerCard(
+                                              quiz: quiz,
+                                              question: quiz.questions![index],
+                                              onRemoveQuestion: () {
+                                                ref
+                                                    .read(editQuizControllerProvider(quiz.id).notifier)
+                                                    .removeQuestion(index: index);
+                                              },
+                                            );
+                                        }
+                                      },
+                                    ),
+                                    const SizedBox(height: 8),
+                                    FloatingActionButton(
+                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(999)),
+                                      onPressed: () {
+                                        showDialog(
+                                          context: context,
+                                          builder: (context) => AddQuestionDialogContent(quiz: quiz),
+                                        );
+                                      },
+                                      child: const Icon(Icons.add),
+                                    ),
+                                    const SizedBox(height: 20),
+                                  ],
+                                ),
                               ),
                             ),
                           ],
                         ),
                       ),
-                      const SizedBox(height: 12),
-                      Container(
-                        constraints: const BoxConstraints(maxWidth: 1000),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(kCardBorderRadius),
-                          child: Column(
-                            children: [
-                              EditQuizTitleCard(quiz: quiz),
-                              const SizedBox(height: 16),
-                              ListView.separated(
-                                shrinkWrap: true,
-                                physics: const NeverScrollableScrollPhysics(),
-                                itemCount: quiz.questions?.length ?? 0,
-                                separatorBuilder: (context, index) => const SizedBox(height: 8),
-                                itemBuilder: (context, index) {
-                                  switch (quiz.questions![index].type) {
-                                    case 'single':
-                                      return EditQuizSingleAnswerCard(
-                                        quiz: quiz,
-                                        question: quiz.questions![index],
-                                        onRemoveQuestion: () {
-                                          ref
-                                              .read(editQuizControllerProvider(quiz.id).notifier)
-                                              .removeQuestion(index: index);
-                                        },
-                                      );
-                                    case 'multiple':
-                                      return EditQuizMultipleAnswerCard(
-                                        quiz: quiz,
-                                        question: quiz.questions![index],
-                                        onRemoveQuestion: () {
-                                          ref
-                                              .read(editQuizControllerProvider(quiz.id).notifier)
-                                              .removeQuestion(index: index);
-                                        },
-                                      );
-                                    default:
-                                      return EditQuizSingleAnswerCard(
-                                        quiz: quiz,
-                                        question: quiz.questions![index],
-                                        onRemoveQuestion: () {
-                                          ref
-                                              .read(editQuizControllerProvider(quiz.id).notifier)
-                                              .removeQuestion(index: index);
-                                        },
-                                      );
-                                  }
-                                },
-                              ),
-                              const SizedBox(height: 8),
-                              FloatingActionButton(
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(999)),
-                                onPressed: () {
-                                  showDialog(
-                                    context: context,
-                                    builder: (context) => AddQuestionDialogContent(quiz: quiz),
-                                  );
-                                },
-                                child: const Icon(Icons.add),
-                              ),
-                              const SizedBox(height: 20),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             );
